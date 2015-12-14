33d2912008adfa005187713820b7b78d60d53589
Nicolas BLANC <nicolas.r.blanc@gmail.com>
2015-12-14 22:56:21 +0100

    Coorection for Obj2,, semaphore -> run

--- TP ProdCons/src/jus/poc/prodcons/obj2/ProdConsSemaphore.java
+++ TP ProdCons/src/jus/poc/prodcons/obj2/ProdConsSemaphore.java
@@ -14,38 +14,67 @@ public class ProdConsSemaphore extends ProdCons {
 	private Sem mutex = new Sem(1); //un semaphore pour l'exclusion mutuelle
 	private Sem semV;
 	
-	public ProdConsSemaphore(int taille) {
-		super(taille);
+	public ProdConsSemaphore(int taille, int nbProd, int nbCons) {
+		super(taille, nbProd, nbCons);
 		semV = new Sem(tailleBuffer); //semaphore pour verifier le nombre de places libres dans le buffer
+	}
+	
+	public void enleverProducteur() {
+		this.nbProd--;
+		if (nbProd == 0) {
+			System.out.println("// ----- ----- \\ Fin du programme : nbprod = " + nbProd + "// ----- ----- \\");
+			this.finProg();
+		}
+	}
+	
+	private synchronized void finProg() {
+		stop = true;
+		System.out.println("// ----- || -----\\ Changer variable stop || mutex : " + mutex.getValeur() + " || semV : " + semV.getValeur() + " || semP : " + semP.getValeur() + " // ----- || -----\\");
+		System.out.println("// ----- || -----\\ ProdConsSemaphore // ----- || -----\\ ");
+		while (nbCons != 0) {
+			semP.V();
+			semV.V();
+			mutex.V();
+		}
 	}
 
 	@Override
 	public Message get(_Consommateur arg0) throws Exception, InterruptedException {
 		semP.P(); //decrementer le nombre de places occupees
 		mutex.P(); // pour l'exclusion mutuelle, semaphore binaire
-
-		Message m = buffer[out];
-		out = (out + 1) % tailleBuffer;
-		
-		nbplein--;
-		
-		mutex.V();
-		semV.V(); //incrementer le nombre de places libres
-		
-		return m;
+		if(!stop) {
+			synchronized (buffer) {	
+				Message m = buffer[out];
+				out = (out + 1) % tailleBuffer;
+				
+				nbplein--;
+				
+				mutex.V();
+				semV.V(); //incrementer le nombre de places libres
+				
+				return m;
+			}
+		} else {
+			return null;
+		}
 	}
+	
 	@Override
 	public void put(_Producteur arg0, Message arg1) throws Exception, InterruptedException {
 		semV.P(); //decrementer le nombre de places libres
 		mutex.P();
-		
-		buffer[in] = arg1;
-		in = (in + 1) % tailleBuffer;
-		
-		nbplein++;
-		
-		mutex.V();
-		semP.V(); //incrementer le nombre de places occupees
+
+		if(!stop) {
+			synchronized (buffer) {
+				buffer[in] = arg1;
+				in = (in + 1) % tailleBuffer;
+				
+				nbplein++;
+				
+				mutex.V();
+				semP.V(); //incrementer le nombre de places occupees
+			}
+		}
 	}
 }
 
--- TP ProdCons/src/jus/poc/prodcons/obj2/TestProdCons.java
+++ TP ProdCons/src/jus/poc/prodcons/obj2/TestProdCons.java
@@ -1,22 +1,100 @@
 package jus.poc.prodcons.obj2;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Map;
+import java.util.Properties;
+
 import jus.poc.prodcons.ControlException;
 import jus.poc.prodcons.Observateur;
 import jus.poc.prodcons.Tampon;
+import jus.poc.prodcons.obj1.Consommateur;
+import jus.poc.prodcons.obj1.Producteur;
 
-public class TestProdCons extends jus.poc.prodcons.obj1.TestProdCons{
+public class TestProdCons extends jus.poc.prodcons.obj1.TestProdCons {
+	
+	protected int nbProd;
+	protected int nbCons;
+	protected int nbBuffer;
+	protected int tempsMoyenProduction;
+	protected int deviationTempsMoyenProduction;
+	protected int tempsMoyenConsommation;
+	protected int deviationTempsMoyenConsommation;
+	protected int nombreMoyenDeProduction;
+	protected int deviationNombreMoyenDeProduction;
+	protected int nombreMoyenNbExemplaire;
+	protected int deviationNombreMoyenNbExemplaire;
+
+	protected  void init(String file) throws InvalidPropertiesFormatException, IOException, IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+		Properties properties = new Properties();
+		properties.loadFromXML(ClassLoader.getSystemResourceAsStream(file));
+		String key;
+		int value;
+		Class<?> thisOne = getClass();
+		for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+			key = (String) entry.getKey();
+			value = Integer.parseInt((String) entry.getValue());
+			thisOne.getDeclaredField(key).set(this, value);
+		}
+	}
+
 	public TestProdCons(Observateur observateur) {
 		super(observateur);
-	}
-
-	protected void run() throws ControlException {
-		Tampon tampon = new ProdConsSemaphore(3);
-		
-		this.createThread(tampon);
-		
-		System.out.println("// ----- ----- \\ Fin de TestProdCons // ----- ----- \\");
+		try {
+			init("jus/poc/prodcons/options/options.xml");
+		} catch (IllegalArgumentException
+				| IllegalAccessException | NoSuchFieldException
+				| SecurityException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
+	protected void createThread(Tampon tampon) throws ControlException {
+		ArrayList<Runnable> producteur = new ArrayList<Runnable>();
+		ArrayList<Runnable> consommateur = new ArrayList<Runnable>();
+		ArrayList<Thread> threadProducteur = new ArrayList<Thread>();
+		ArrayList<Thread> threadConsommateur = new ArrayList<Thread>();
+		
+		for (int i = 0; i < nbProd; i++) {
+			producteur.add(new Producteur(tampon, observateur, tempsMoyenProduction, deviationTempsMoyenProduction, nombreMoyenDeProduction, deviationNombreMoyenDeProduction));
+		}
+		
+		for (int i = 0; i < nbCons; i++) {
+			consommateur.add(new Consommateur(tampon, observateur, tempsMoyenConsommation, deviationTempsMoyenConsommation));
+		}
+		
+		for (Runnable p : producteur) {
+			threadProducteur.add(new Thread(p));
+		}
+		
+		for (Runnable c : consommateur) {
+			threadProducteur.add(new Thread(c));
+		}
+		
+		for (Thread thread : threadProducteur) {
+			thread.start();
+		}
+		
+		for (Thread thread : threadConsommateur) {
+			thread.start();
+		}
+	}
+
+	/**
+	 * Initialise selon le fichier option,
+	 * puis créé le buffer, avec un certain nombre d'espace libre
+	 * et enfin créé un certain nombre de thread producteur et consommateur
+	 */
+	protected void run() throws Exception {		
+		Tampon tampon = new ProdConsSemaphore(nbBuffer, nbProd, nbCons);
+		
+		this.createThread(tampon);
+
+		System.out.println("// ----- ----- \\ Fin de TestProdCons // ----- ----- \\");
+	}
+
 	public static void main(String[] args) {
 		System.out.println("// ----- ----- \\ Lancement programme : Obj2 // ----- ----- \\");
 		new TestProdCons(new Observateur()).start();
